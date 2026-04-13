9e7974f37e84bfa91a57c0380419dc5f9e0a4651
Sergio <sasellers01@educarex.es>
2026-04-13 19:27:05 +0200

    Refactorizar HTML y añadir CSS y JS básico

--- fuentes/js/script.js
+++ fuentes/js/script.js
@@ -1,17 +1,9 @@
-'use strict'
-
-const botonInsertar = document.querySelectorAll('#botonInsertar')[0]
-
-const inputNombre = document.querySelectorAll('#nombre')[0]
-const inputFecha = document.querySelectorAll('#fecha')[0]
-const inputTipo = document.querySelectorAll('#tipo')[0]
-
-botonInsertar.addEventListener( 'click', insertar )
-
-function insertar() {
-	console.log("Vamos a insertar los datos del Armagedón")
-	const nombre = inputNombre.value
-	const fecha = inputFecha.value
-	const armagedon = new Armagedon(nombre, fecha, null)
-	console.log(armagedon)
-}
+document.querySelector("form").addEventListener("submit", function(e) {
+	e.preventDefault();
+	
+	let nombre = document.getElementById("nombre").value;
+	let cantidad = document.getElementById("descripcion").value;
+	
+	console.log(nombre);
+	console.log(descripcion);
+});
