b27a0ef58682a0d487bc4bcee20add7407a83a0a
javierbg0404-cloud <javierbg0404@gmail.com>
2026-04-13 19:41:33 +0200

    Delete fuentes/js/script.js

--- fuentes/js/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("form").addEventListener("submit", function(e) {
-	e.preventDefault();
-	
-	let nombre = document.getElementById("nombre").value;
-	let cantidad = document.getElementById("descripcion").value;
-	
-	console.log(nombre);
-	console.log(descripcion);
-});
